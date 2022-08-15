96a7d7eaf735c9f6cb5850ed1ad72db0b9d551d5
lemon-mint <ice.lemons.mint@gmail.com>
2022-08-15 22:52:28 +0900

    Remove src Clean Code

--- gen.go
+++ gen.go
@@ -2,5 +2,4 @@ package main
 
 //go:generate go install github.com/goccmack/gogll/v3@latest
 //go:generate go run ./tools/clean script
-//go:generate go run ./tools/clean src
 //go:generate gogll -o script -go confscript.md
