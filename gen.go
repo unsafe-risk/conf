7370473da316aeb6a0cdeb6dd688708c2ddf90b6
lemon-mint <ice.lemons.mint@gmail.com>
2022-08-15 22:52:09 +0900

    Remove Rust Code

--- gen.go
+++ gen.go
@@ -4,4 +4,3 @@ package main
 //go:generate go run ./tools/clean script
 //go:generate go run ./tools/clean src
 //go:generate gogll -o script -go confscript.md
-//go:generate gogll -rust confscript.md
