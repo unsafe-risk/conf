422a41bf8001a452393d1b901e8e6ca49a021bce
lemon-mint <ice.lemons.mint@gmail.com>
2022-08-15 22:13:32 +0900

    Fix Path

--- gen.go
+++ gen.go
@@ -2,5 +2,6 @@ package main
 
 //go:generate go install github.com/goccmack/gogll/v3@latest
 //go:generate go run ./tools/clean script
+//go:generate go run ./tools/clean src
 //go:generate gogll -o script -go confscript.md
-//go:generate gogll -rust -o src/parser -go confscript.md
+//go:generate gogll -rust confscript.md
